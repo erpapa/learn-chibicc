aa50d17aaf17e84061d43b0f53465ba477d6ee8b
erpapa <hyplcf@gmail.com>
2023-10-31 11:31:09 +0800

    创建一个编译一个整数的编译器

---
+++ README.md
@@ -0,0 +1,4 @@
+# learn-chibicc
+
+Learning chibicc from https://www.sigbus.info/compilerbook
+
--- chibicc/main.c
+++ chibicc/main.c
@@ -6,9 +6,21 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    if (argc != 2) {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+
+    // macOS中，汇编语言程序执行的起点是_main函数。
+    // windows中，汇编语言程序执行的起点是_start函数。
+    // linux中，汇编语言程序执行的起点是main函数。
+    printf(".intel_syntax noprefix\n");
+    printf(".global _main\n");
+    printf("_main:\n");
+    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  ret\n");
     return 0;
 }
