bd87c1264745534cb3b59449635e960271445353
erpapa <hyplcf@gmail.com>
2023-11-07 14:09:20 +0800

    Accept multiple statements separated by semicolons

--- chibicc/codegen.c
+++ chibicc/codegen.c
@@ -68,12 +68,23 @@ static void gen_expr(Node *node) {
     error("invalid expression");
 }
 
+static void gen_stmt(Node *node) {
+    if (node->kind == ND_EXPR_STMT) {
+        gen_expr(node->lhs);
+        return;
+    }
+    
+    error("invalid statement");
+}
+
 void codegen(Node *node) {
     printf("  .globl _main\n");
     printf("_main:\n");
+        
+    for (Node *n = node; n; n = n->next) {
+        gen_stmt(n);
+        assert(depth == 0);
+    }
     
-    gen_expr(node);
-    printf("  ret\n");
-    
-    assert(depth == 0);
+    printf(" ret\n");
 }
